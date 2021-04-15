b0f2c3b0f108586ad3fd6230e15b73444d6408d8
Rishikant Sahu <rishikantsahu181@gmail.com>
2021-04-15 16:33:53 +0530

    A basic HW stats monitor

--- Program.cs
+++ Program.cs
@@ -2,10 +2,82 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using OpenHardwareMonitor;
+using OpenHardwareMonitor.Hardware;
 
 namespace ArduinoHWMonitor {
     class Program {
+        // Member data
+        Computer thisComputer;                              // To store the reference to this computer
         
+        // Temperature stats
+        String cpuTemp;                                     // To store CPU temperature
+        String gpuTemp;                                     // To store GPU temperature
+
+        // Usage stats
+        String cpuUsage;                                    // To store CPU usage
+        String gpuUsage;                                    // To store GPU usage
+
+        // Member method
+        // Constructor
+        Program() {
+            // Creating new connection to get GPU and CPU data
+            thisComputer = new Computer() { CPUEnabled = true, GPUEnabled = true };
+
+            // Opening connection
+            thisComputer.Open();
+        }
+
+        // Method to get CPU and GPU stats
+        private String getStats() {
+            // Getting each hardware item
+            foreach(var hwItem in thisComputer.Hardware) {
+                // Checking if hardware is CPU
+                if(hwItem.HardwareType == HardwareType.CPU) {
+                    // Updating to get newest stats
+                    hwItem.Update();
+                    // Getting each stat
+                    foreach(var sensor in hwItem.Sensors) {
+                        // For temp
+                        if(sensor.SensorType == SensorType.Temperature) {
+                            cpuTemp = sensor.Value.Value.ToString();
+                        }
+                        // For usage
+                        else if(sensor.SensorType == SensorType.Load) {
+                            cpuUsage = sensor.Value.Value.ToString();
+                        }
+                    }
+                }
+                // Checking if hardware is GPU
+                else if (hwItem.HardwareType == HardwareType.GpuNvidia) {
+                    // Updating to get newest stats
+                    hwItem.Update();
+                    // Getting each stat
+                    foreach (var sensor in hwItem.Sensors) {
+                        // For temp
+                        if (sensor.SensorType == SensorType.Temperature) {
+                            gpuTemp = sensor.Value.Value.ToString();
+                        }
+                        // For usage
+                        else if (sensor.SensorType == SensorType.Load) {
+                            gpuUsage = sensor.Value.Value.ToString();
+                        }
+                    }
+                }
+            }
+
+            // Returning data
+            return (cpuTemp + "\t\t" + cpuUsage + "\n" + gpuTemp + "\t\t" + gpuUsage);
+        }
+
+        // Main method
+        public static void Main(String[] args) {
+            Program ob = new Program();
+            while(true) {
+                Console.WriteLine(ob.getStats());
+                Thread.Sleep(1000);
+            }
+        }
     }
 }
